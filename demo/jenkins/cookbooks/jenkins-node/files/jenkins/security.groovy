8e28284bc151ce5278050914788d2f75728adada
Jason Baker <jasondbaker@hotmail.com>
2018-05-10 20:23:05 -0500

    update jenkins security

--- demo/jenkins/cookbooks/jenkins-node/files/jenkins/security.groovy
+++ demo/jenkins/cookbooks/jenkins-node/files/jenkins/security.groovy
@@ -24,3 +24,24 @@ instance.save()
 Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
 
 jenkins.model.Jenkins.instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
+
+/*
+   Disable all JNLP protocols except for JNLP4.  JNLP4 is the most secure agent
+   protocol because it is using standard TLS.
+*/
+Jenkins j = Jenkins.instance
+
+if(!j.isQuietingDown()) {
+    Set<String> agentProtocolsList = ['JNLP4-connect', 'Ping']
+    if(!j.getAgentProtocols().equals(agentProtocolsList)) {
+        j.setAgentProtocols(agentProtocolsList)
+        println "Agent Protocols have changed.  Setting: ${agentProtocolsList}"
+        j.save()
+    }
+    else {
+        println "Nothing changed.  Agent Protocols already configured: ${j.getAgentProtocols()}"
+    }
+}
+else {
+    println 'Shutdown mode enabled.  Configure Agent Protocols SKIPPED.'
+}
